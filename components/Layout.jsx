6d5aa181c5629372dfd3f4eb837bc0eb981d3926
Kartikeya Batura <33941035+k-batura@users.noreply.github.com>
2022-05-08 10:52:26 -0400

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import Head from 'next/head';
+import React from "react";
+import Head from "next/head";
 
-import Footer from './Footer';
-import Navbar from './Navbar';
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
-const Layout = ( {children} ) => {
+const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>Karti's Ecommerce Store</title>
       </Head>
 
       <header>
         <Navbar />
       </header>
 
-      <main className="main-container">
-        {children}
-      </main>
+      <main className="main-container">{children}</main>
 
       <footer>
         <Footer />
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
